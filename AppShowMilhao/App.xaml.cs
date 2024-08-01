b793c268dcd7c182e88d9643fb02532363ac7b53
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-08-01 15:56:19 -0300

    Inicio

--- AppShowMilhao/App.xaml.cs
+++ AppShowMilhao/App.xaml.cs
@@ -1,7 +1,18 @@
-﻿namespace AppShowMilhao
+﻿using AppShowMilhao.Models;
+
+namespace AppShowMilhao
 {
     public partial class App : Application
     {
+        List<Pergunta> perguntas_faceis = new()
+        {
+            new Pergunta
+            {
+                Id = 1,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?"
+            }
+        };
+
         public App()
         {
             InitializeComponent();
